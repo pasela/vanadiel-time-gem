a1d233fdcbf9f5f5ccaac1f8d0d7a9422177a3fb
Yuki <paselan@gmail.com>
2012-08-23 02:41:09 +0900

    Add encoding magic comment

--- lib/vanadiel/day.rb
+++ lib/vanadiel/day.rb
@@ -1,14 +1,16 @@
+# encoding: utf-8
+
 module Vanadiel
   # weekdays
   module Day
-    FIRESDAY      = 0
-    EARTHSDAY     = 1
-    WATERSDAY     = 2
-    WINDSDAY      = 3
-    ICEDAY        = 4
-    LIGHTNINGDAY  = 5
-    LIGHTSDAY     = 6
-    DARKSDAY      = 7
+    FIRESDAY      = 0 # 火曜日
+    EARTHSDAY     = 1 # 土曜日
+    WATERSDAY     = 2 # 水曜日
+    WINDSDAY      = 3 # 風曜日
+    ICEDAY        = 4 # 氷曜日
+    LIGHTNINGDAY  = 5 # 雷曜日
+    LIGHTSDAY     = 6 # 光曜日
+    DARKSDAY      = 7 # 闇曜日
 
     DAYNAMES = %w(Firesday Earthsday Watersday Windsday Iceday Lightningday Lightsday Darksday)
   end
--- lib/vanadiel/moon.rb
+++ lib/vanadiel/moon.rb
@@ -1,25 +1,19 @@
+# encoding: utf-8
+
 module Vanadiel
-  # moon ages
+  # moon phase
   module Moon
-    NEW_MOON           = 0  # 新月
-    CRESCENT_MOON      = 1  # 三日月
-    WAXING_CRESCENT1   = 1  # 三日月
-    SIX_AGE_MOON       = 2  # 七日月
-    WAXING_CRESCENT2   = 2  # 七日月
-    FIRST_QUARTER      = 3  # 上弦の月
-    NINE_AGE_MOON      = 4  # 十日夜
-    WAXING_GIBBOUS1    = 4  # 十日夜
-    GIBBOUS_MOON       = 5  # 十三夜
-    WAXING_GIBBOUS2    = 5  # 十三夜
-    FULL_MOON          = 6  # 満月
-    FIFTEEN_AGE_MOON   = 7  # 十六夜
-    WANING_GIBBOUS1    = 7  # 十六夜
-    DISSEMINATING_MOON = 8  # 居待月
-    WANING_GIBBOUS2    = 8  # 居待月
-    LAST_QUARTER       = 9  # 下弦の月
-    TWENTY_AGE_MOON    = 10 # 二十日余月
-    WANING_CRESCENT1   = 10 # 二十日余月
-    BALSAMIC_MOON      = 11 # 二十六夜
-    WANING_CRESCENT2   = 11 # 二十六夜
+    NEW_MOON              = 0  # 新月
+    WAXING_CRESCENT_MOON1 = 1  # 三日月
+    WAXING_CRESCENT_MOON2 = 2  # 七日月
+    FIRST_QUARTER_MOON    = 3  # 上弦の月
+    WAXING_GIBBOUS_MOON1  = 4  # 十日夜
+    WAXING_GIBBOUS_MOON2  = 5  # 十三夜
+    FULL_MOON             = 6  # 満月
+    WANING_GIBBOUS_MOON1  = 7  # 十六夜
+    WANING_GIBBOUS_MOON2  = 8  # 居待月
+    LAST_QUARTER_MOON     = 9  # 下弦の月
+    WANING_CRESCENT_MOON1 = 10 # 二十日余月
+    WANING_CRESCENT_MOON2 = 11 # 二十六夜
   end
 end
